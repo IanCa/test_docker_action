957e130555593a5c916a25dd8e5bd8cc16390ed4
IanCa <ianrcallanan@gmail.com>
2024-02-14 14:20:16 -0600

    Add a print

--- test_service.py
+++ test_service.py
@@ -5,6 +5,7 @@ def test_hello_world():
     response = requests.get(url)
     assert response.status_code == 200
     assert response.text == "Hello, World!"
+    print("The function ran successfully")
 
 if __name__ == "__main__":
     test_hello_world()
