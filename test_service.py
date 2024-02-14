f6f50566a012a2f5b7cf59e6aa5a12b8becbeb73
IanCa <ianrcallanan@gmail.com>
2024-02-14 14:19:14 -0600

    See if this workflow works at all

---
+++ test_service.py
@@ -0,0 +1,10 @@
+import requests
+
+def test_hello_world():
+    url = "http://localhost:5000/"
+    response = requests.get(url)
+    assert response.status_code == 200
+    assert response.text == "Hello, World!"
+
+if __name__ == "__main__":
+    test_hello_world()
